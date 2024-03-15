1dfa6f59309f00c43bdaa145c4ce75ee8e2e1827
HarshTomarinfinity <119696390+HarshTomarinfinity@users.noreply.github.com>
2024-03-15 08:47:17 +0530

    commit

--- Day-3/love10.cpp
+++ Day-3/love10.cpp
@@ -1,18 +1,29 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     int num;
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int j=arr[0];
+//     for(int i=1;i<n;i++){
+//         j=j&&arr[i];
+//     }
+    
+//     cout<<j;
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    int num;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int j=arr[0];
-    for(int i=1;i<n;i++){
-        j=j&&arr[i];
-    }
-    cout<<j;
+
     return 0;
 }
