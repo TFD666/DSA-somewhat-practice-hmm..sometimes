bef777a23a333c327c15e47024087d9eca474b90
HarshTomarinfinity <119696390+HarshTomarinfinity@users.noreply.github.com>
2024-04-03 11:11:40 +0530

    commit

--- Day-4/Array_part3.cpp
+++ Day-4/Array_part3.cpp
@@ -1,38 +1,119 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[3][3];
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+//     cout<<"Before: "<<endl;
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<arr[i][j]<<" ";
+            
+//         }
+//         cout<<endl;
+//     }
+//     int temp[3][3];
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             temp[i][j]=arr[i][j];
+//         }
+//     }
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             arr[j][3-1-i]=temp[i][j];
+//         }
+//     }
+//     cout<<"after"<<endl;
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//move all negative to left side
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     vector<int>a;
+//     for(int i=0;i<n;i++){
+//         int c;
+//         cin>>c;
+//         a.push_back(c);
+//     }
+    
+//     int first=0;
+//     int last =n-1;
+//     while(first<last){
+//         if(a[first]<0){
+//             first++;
+//         }else if(a[last]>0){
+//             last--;
+//         }else{
+//             swap(a[first],a[last]);
+//         }
+//     }
+//     for(int i=0;i<a.size();i++){
+//         cout<<a[i];
+//     }
+//     return 0;
+// }
+
+
+//dublicate number
+// #include<iostream>
+// #include<vector>
+// #include <bits/stdc++.h> 
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     vector<int>a;
+//     for(int i=0;i<n;i++){
+//         int c;
+//         cin>>c;
+//         a.push_back(c);
+//     }
+//     sort(a.begin(),a.end());
+//     for(int i=0;i<n-1;i++){
+//         if(a[i]==a[i+1]){
+//             cout<<a[i];
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
 #include<iostream>
+#include<vector>
+#include <bits/stdc++.h> 
 using namespace std;
 int main()
 {
-    int arr[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cin>>arr[i][j];
-        }
+    int n;
+    cin>>n;
+    vector<int>a;
+    for(int i=0;i<n;i++){
+        int c;
+        cin>>c;
+        a.push_back(c);
     }
-    cout<<"Before: "<<endl;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cout<<arr[i][j]<<" ";
-            
-        }
-        cout<<endl;
+    int first=a[0];
+    while(first!=a[first]){
+        swap(a[0],a[first]);
     }
-    int temp[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            temp[i][j]=arr[i][j];
-        }
-    }
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            arr[j][3-1-i]=temp[i][j];
-        }
-    }
-    cout<<"after"<<endl;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    cout<<a[first];
+
     return 0;
 }
