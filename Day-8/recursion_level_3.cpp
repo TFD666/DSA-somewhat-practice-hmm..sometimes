29e17e2386fc56360dd33b7b5aceed7a459d47af
TFD <twofaceddevil666@gmail.com>
2024-06-30 02:55:52 +0530

    commit

--- Day-8/recursion_level_3.cpp
+++ Day-8/recursion_level_3.cpp
@@ -29,32 +29,66 @@
 // }
 
 
+// #include<iostream>
+// #include<vector>
+// #include<limits>
+// using namespace std;
+// int coinsum(vector<int> arr,int target){
+//     if(target==0){
+//         return 0;
+//     }
+//     if(target<0){
+//         return INT32_MAX;
+//     }
+//     int mini=INT32_MAX;
+//     for(int i=0;i<arr.size();i++){
+//         int ans=coinsum(arr,target-arr[i]);
+//         if(ans!=INT32_MAX){
+//             mini=min(mini,ans+1);
+//         }
+//     }
+//     return mini;
+    
+// }
+// int main()
+// {
+//     vector<int>arr={1,2,3};
+//     int sum=0,target=13;
+//     int ans=coinsum(arr,target);
+//     cout<<ans;
+//     return 0;
+// }
+
+
+
 #include<iostream>
 #include<vector>
 #include<limits>
 using namespace std;
-int coinsum(vector<int> arr,int target){
+int linesegment(vector<int> arr,int target){
     if(target==0){
         return 0;
     }
     if(target<0){
         return INT32_MAX;
     }
-    int mini=INT32_MAX;
+    int count=0;
     for(int i=0;i<arr.size();i++){
-        int ans=coinsum(arr,target-arr[i]);
+        int ans=linesegment(arr,target-arr[i]);
         if(ans!=INT32_MAX){
-            mini=min(mini,ans+1);
+            count++;
         }
     }
-    return mini;
+    return count;
     
 }
 int main()
 {
-    vector<int>arr={1,2,3};
-    int sum=0,target=13;
-    int ans=coinsum(arr,target);
+    vector<int>arr={5,2,2};
+    int line=7;
+    int ans=linesegment(arr,line);
     cout<<ans;
     return 0;
 }
+
+
