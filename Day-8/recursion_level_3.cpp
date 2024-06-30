7c784bda7632964a03b7c896a6588d21e9b3b903
TFD <twofaceddevil666@gmail.com>
2024-07-01 04:47:05 +0530

    commit

--- Day-8/recursion_level_3.cpp
+++ Day-8/recursion_level_3.cpp
@@ -61,33 +61,49 @@
 
 
 
+// #include<iostream>
+// #include<vector>
+// #include<limits>
+// using namespace std;
+// int linesegment(vector<int> arr,int target){
+//     if(target==0){
+//         return 0;
+//     }
+//     if(target<0){
+//         return INT32_MAX;
+//     }
+//     int count=0;
+//     for(int i=0;i<arr.size();i++){
+//         int ans=linesegment(arr,target-arr[i]);
+//         if(ans!=INT32_MAX){
+//             count++;
+//         }
+//     }
+//     return count;
+    
+// }
+// int main()
+// {
+//     vector<int>arr={5,2,2};
+//     int line=7;
+//     int ans=linesegment(arr,line);
+//     cout<<ans;
+//     return 0;
+// }
+
 #include<iostream>
 #include<vector>
-#include<limits>
 using namespace std;
-int linesegment(vector<int> arr,int target){
-    if(target==0){
-        return 0;
-    }
-    if(target<0){
-        return INT32_MAX;
-    }
-    int count=0;
-    for(int i=0;i<arr.size();i++){
-        int ans=linesegment(arr,target-arr[i]);
-        if(ans!=INT32_MAX){
-            count++;
-        }
-    }
-    return count;
-    
+int rober(vector<int>&arr){
+    // if(arr>=arr.size()){
+    //     return;
+    // }
 }
 int main()
 {
-    vector<int>arr={5,2,2};
-    int line=7;
-    int ans=linesegment(arr,line);
-    cout<<ans;
+    vector<int> arr={1,2,3,7};
+    cout<<&arr;
+    // cout<<rober(arr);
     return 0;
 }
 
