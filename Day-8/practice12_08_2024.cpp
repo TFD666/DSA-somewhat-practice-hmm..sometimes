1e3d06f64de209a942037c9fe17e30f4faa0a18d
TFD666 <twofaceddevil666@gmail.com>
2024-08-15 17:06:41 +0530

    commit

---
+++ Day-8/practice12_08_2024.cpp
@@ -0,0 +1,27 @@
+//checking palindrome of avector
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// bool palindromecheck(vector<int>&v,int i,int n){
+//     if(i>=n){
+//         return true;
+//     }
+//     if(v[i]!=v[n]){
+//         return false;
+//     }
+//     return palindromecheck(v,i+1,n-1);
+// }
+// int main()
+// {
+//     vector<int>v={1};
+//     int i=0,n=v.size()-1;
+//     cout<<palindromecheck(v,i,n);
+//     return 0;
+// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
