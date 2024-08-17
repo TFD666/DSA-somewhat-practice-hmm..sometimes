bf477a92df962b39829e7e48be8bffd24b96dc82
TFD666 <twofaceddevil666@gmail.com>
2024-08-17 20:20:38 +0530

    commit

--- Day-8/practice12_08_2024.cpp
+++ Day-8/practice12_08_2024.cpp
@@ -69,30 +69,81 @@
 //     }
 // };
 
+//this one displays the substring of a array using recurtion
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void printsubstringpart(vector<int>&arr,int st,int ed){
+//     if(ed>=arr.size()){
+//         return ;
+//     }
+//     for(int i=st;i<=ed;i++){
+//         cout<<arr[i];
+//     }
+//     cout<<endl;
+//     printsubstringpart(arr,st,ed+1);
+// }
+// void printsubstring(vector<int>&arr){
+//     for(int start=0;start<arr.size();start++){
+//         int end=start;
+//         printsubstringpart(arr,start,end);
+//     }
+// }
+// int main()
+// {
+//     vector<int>arr={1,2,3,4,5};
+//     int st=0,ed=0;
+//     printsubstring(arr);
+//     return 0;
+// }
 
-#include<iostream>
-#include<vector>
-using namespace std;
-void printsubstringpart(vector<int>&arr,int st,int ed){
-    if(ed>=arr.size()){
-        return ;
-    }
-    for(int i=st;i<=ed;i++){
-        cout<<arr[i];
-    }
-    cout<<endl;
-    printsubstringpart(arr,st,ed+1);
-}
-void printsubstring(vector<int>&arr){
-    for(int start=0;start<arr.size();start++){
-        int end=start;
-        printsubstringpart(arr,start,end);
-    }
-}
-int main()
-{
-    vector<int>arr={1,2,3,4,5};
-    int st=0,ed=0;
-    printsubstring(arr);
-    return 0;
-}
+//this one is the buy and sell stocs problem but the solution is correct but it will give a time exceed error so the t=right solution is down
+// #include<iostream>
+// #include <algorithm>
+// #include<vector>
+// using namespace std;
+// void minel(vector<int>&arr){
+
+// }
+// int main()
+// {
+//     vector<int> arr={2,4,1};
+//     int min=INT32_MAX;
+//     int max=INT32_MIN;
+//     int profit=INT32_MIN;
+//     for(int i=0;i<arr.size();i++){
+//         for(int j=i+1;j<arr.size();j++){
+//             if(arr[j]-arr[i]>=profit){
+//                 min=arr[i];
+//                 max=arr[j];
+//                 profit=max-min;
+//             }
+//         }
+//     }
+//     cout<<profit;
+
+//     return 0;
+// }
+
+
+// class Solution {
+// public:
+//     void mpf(vector<int>& arr,int i,int &min,int &max_profit){
+//         if(i==arr.size()){
+//             return;
+//         }
+
+//         if(arr[i]<min) min=arr[i];
+//         int today=arr[i]-min;
+//         if(today>max_profit) max_profit=today;
+//         return mpf(arr,i+1,min,max_profit);
+//     }
+//     int maxProfit(vector<int>& arr) {
+//         int min=INT_MAX;
+//         int max_profit=INT_MIN;
+//         mpf(arr,0,min,max_profit);
+//         return max_profit;
+//     }
+// };
+
+
