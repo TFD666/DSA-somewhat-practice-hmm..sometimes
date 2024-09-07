be4327a54d9240bee31148cb2a0ee993578657ab
TFD666 <twofaceddevil666@gmail.com>
2024-09-07 14:07:10 +0530

    commit

--- Day-10/DNC1.cpp
+++ Day-10/DNC1.cpp
@@ -34,6 +34,47 @@
 //divide and concquer usin grecursion in merge sort
 #include<iostream>
 using namespace std;
+void  merge(int *arr,int s,int e ){
+    //base case
+    
+    int mid=(s+e)/2;
+    int length1=mid-s+1;
+    int length2=e-mid;
+    //making a new arrays to copy elements
+    int *left=new int[length1];
+    int *right=new int[length2];
+    //coping elemets
+    int k=s;
+    for(int i=0;i<length1;i++){
+        left[i]=arr[k];
+        k++;
+    }
+    k=mid+1;
+    for(int i=0;i<length2;i++){
+        right[i]=arr[k];
+        k++;
+    }
+    //sorting them;
+    int leftindex=0;
+    int rightindex=0;
+    int mainindex=s;
+    while(leftindex<length1&&rightindex<length2){
+        if(left[leftindex]<right[rightindex]){
+            arr[mainindex++]=left[leftindex++];
+        }
+        else{
+            arr[mainindex++]=right[rightindex++];
+        }
+    }
+    //remaing elements if there are any
+    while(leftindex<length1){
+        arr[mainindex++]=left[leftindex++];
+    }
+    
+    while(rightindex<length2){
+        arr[mainindex++]=right[rightindex++];
+    }
+}
 void mergesort(int *arr,int s,int e){
     //base case
     if(s>=e){
@@ -45,10 +86,8 @@ void mergesort(int *arr,int s,int e){
     mergesort(arr,s,mid);
     //right part
     mergesort(arr,mid+1,e);
-
-    merge()
-
-
+    //merge th eelememts
+    merge(arr,s,e);
 
 }
 int main()
@@ -57,5 +96,8 @@ int main()
     int n=5;
     int s=0,e=n-1;
     mergesort(arr,s,e);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
