a995f5a3594d8dc2d3618e3e361aceed5d84877f
TFD666 <twofaceddevil666@gmail.com>
2024-09-20 14:43:16 +0530

    commit

--- Day-10/DNC2.cpp
+++ Day-10/DNC2.cpp
@@ -57,49 +57,50 @@
 
 
 //a slightly different way to do quick soert without using count
-#include<iostream>
-using namespace std;
-int partition(int arr[],int s,int e){
-    //choosing the pivot element
-    int PI=s;
-    int PE=arr[s];
-    int i=s+1;
-    int j=e;
-    while(i<j){
-        while (i <= e && arr[i] <= PE) {  
-            i++;  
-        }  
-        // Move j to the left as long as the element is greater than PE  
-        while (j >= s && arr[j] > PE) {  
-            j--;  
-        } 
-        if(i<j){
-            swap(arr[i],arr[j]);
-        }
-    }
-    swap(arr[s],arr[j]);
-    return j;
-}
-void quick_sort(int arr[],int s, int e){
-    if(s>=e){
-        return ;
-    }
-    //partition logic
-    int p=partition(arr,s,e);
-    //sorting left part
-    quick_sort(arr,s,p-1);
+// #include<iostream>
+// using namespace std;
+// int partition(int arr[],int s,int e){
+//     //choosing the pivot element
+//     int PI=s;
+//     int PE=arr[s];
+//     int i=s+1;
+//     int j=e;
+//     while(i<j){
+//         while (i <= e && arr[i] <= PE) {  
+//             i++;  
+//         }  
+//         // Move j to the left as long as the element is greater than PE  
+//         while (j >= s && arr[j] > PE) {  
+//             j--;  
+//         } 
+//         if(i<j){
+//             swap(arr[i],arr[j]);
+//         }
+//     }
+//     swap(arr[s],arr[j]);
+//     return j;
+// }
+// void quick_sort(int arr[],int s, int e){
+//     if(s>=e){
+//         return ;
+//     }
+//     //partition logic
+//     int p=partition(arr,s,e);
+//     //sorting left part
+//     quick_sort(arr,s,p-1);
 
-    //sorting right part
-    quick_sort(arr,p+1,e);
+//     //sorting right part
+//     quick_sort(arr,p+1,e);
     
-}
-int main()
-{
-    int arr[]={11,3,8,20,80,6,3,27};
-    int s=0,e=8-1;
-    quick_sort(arr,s,e);
-    for(int i=0;i<8;i++){
-        cout<<arr[i]<<" ";
-    }
-    return 0;
-}
+// }
+// int main()
+// {
+//     int arr[]={11,3,8,20,80,6,3,27};
+//     int s=0,e=8-1;
+//     quick_sort(arr,s,e);
+//     for(int i=0;i<8;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     return 0;
+// }
+
