2643c12701f8504b6bed19bc3f93aa95352db645
TFD666 <twofaceddevil666@gmail.com>
2024-11-19 18:29:36 +0530

    commit

---
+++ Day-11/revise.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int maxel(vector<int>arr,int maxi){
+//     for(int i=0;i<arr.size();i++){
+//         if(arr[i]>maxi){
+//             maxi=arr[i];
+//         }
+//     }
+//     return maxi;
+
+// }
+// int manel(vector<int>arr,int mini){
+//     for(int i=0;i<arr.size();i++){
+//         if(arr[i]<mini){
+//             mini=arr[i];
+//         }
+//     }
+//     return mini;
+
+// }
+// int main()
+// {
+//     vector<int> arr={2,4,12,3,49,12,43};
+//     int maxi=INT8_MIN;
+//     int mini=INT16_MAX;
+//     cout<<maxel(arr,maxi)<<endl;
+//     cout<<manel(arr,mini);
+
+//     return 0;
+// }
